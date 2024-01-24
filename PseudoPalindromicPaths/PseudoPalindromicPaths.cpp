d0a34abbd9e83258b87abd0a305f9e932de8f37e
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2024-01-24 19:57:52 +0100

    should write a script for this

--- PseudoPalindromicPaths/PseudoPalindromicPaths.cpp
+++ PseudoPalindromicPaths/PseudoPalindromicPaths.cpp
@@ -16,7 +16,7 @@ public:
             return;
         path ^= (1 << root->val);
         if (root->left == NULL && root->right == NULL){
-            if ((path & (path - 1)) == 0)
+            if ((path & (path - 1)) == 0)//pls dont ask why
                 counter++;
             return;
         }

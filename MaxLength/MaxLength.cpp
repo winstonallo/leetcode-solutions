4062f292ae4e66b0d9f8ffc0235c5b2b4380d631
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-01-26 13:38:54 +0100

    Update MaxLength.cpp

--- MaxLength/MaxLength.cpp
+++ MaxLength/MaxLength.cpp
@@ -2,22 +2,22 @@ class Solution{
 public:
     int maxLength(vector<string> &arr){
         int n = arr.size();
-        int maxCombination = (1 << n);//(pow(2, n))
+        int maxCombination = (1 << n);
         int ans = 0;
         for (int i = 0; i < maxCombination; i++){
-            int charMask = 0; //mask for chars in current combination
+            int charMask = 0;
             int len = 0; // current result length
             bool isValid = true;
             for (int j = 0; j < n; j++){
                 if ((i & (1 << j)) == 0)
-                    continue ; // skip if string no. j is not in current combination
+                    continue ;
                 for (char c : arr[j]){
                     int charIndex = c - 'a';
-                    if ((charMask & (1 << charIndex)) != 0){ //check if char is set in mask
+                    if ((charMask & (1 << charIndex)) != 0){
                         isValid = false;
                         break ;
                     }
-                    charMask |= (1 << charIndex); //add char to mask
+                    charMask |= (1 << charIndex);
                     len++;
                 }
                 if (isValid == false)

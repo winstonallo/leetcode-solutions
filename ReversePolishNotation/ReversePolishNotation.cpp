55bcb8e709ce12ccc0e3df0e5279376faf12e691
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2024-01-30 12:37:37 +0100

    30.01.2024

---
+++ ReversePolishNotation/ReversePolishNotation.cpp
@@ -0,0 +1,33 @@
+class Solution {
+
+    public:
+        int evalRPN(vector<string>& tokens) {
+            stack<int> stk;
+            int n = tokens.size();
+
+            for (int i = 0; i < n; i++) {
+                if (operand(tokens[i])) {
+                    int num2 = stk.top();
+                    stk.pop();
+                    int num1 = stk.top();
+                    stk.pop();
+                    if (tokens[i] == "+")
+                        stk.push(num1 + num2);
+                    else if (tokens[i] == "-")
+                        stk.push(num1 - num2);
+                    else if (tokens[i] == "*")
+                        stk.push(num1 * num2);
+                    else if (tokens[i] == "/")
+                        stk.push(num1 / num2);
+                } 
+                else
+                    stk.push(stoi(tokens[i]));
+            }
+            return stk.top();
+        }
+
+    private:
+        bool    operand(string s) {
+            return s == "*" || s == "/" || s == "+" || s == "-";
+        }
+};

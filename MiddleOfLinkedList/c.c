2b13977f501d6865e0a1c52237f4a9a6b536f3ee
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-03-07 08:36:22 +0100

    7-3-24 c

---
+++ MiddleOfLinkedList/c.c
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* middleNode(struct ListNode* head) 
+{
+    struct ListNode *left = head, *right = head;
+
+    while (right != NULL && right->next != NULL)
+    {
+        left = left->next;
+        right = right->next->next;
+    }    
+
+    return left;
+}

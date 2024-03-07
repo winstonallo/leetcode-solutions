4b5778fd2a660c1f3aba722fafae1ce5b6fb2de8
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-03-07 08:42:37 +0100

    7-3-24 go

---
+++ MiddleOfLinkedList/go.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+    left := head;
+    right := head;
+
+    for right != nil && right.Next != nil {
+        left = left.Next;
+        right = right.Next.Next;
+    }
+
+    return left;
+}

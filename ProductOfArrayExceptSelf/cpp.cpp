8c465e815163a5f6a5556ff1bd26d61e52ccea1f
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-03-15 12:25:42 +0100

    optimized for space

--- ProductOfArrayExceptSelf/cpp.cpp
+++ ProductOfArrayExceptSelf/cpp.cpp
@@ -4,25 +4,21 @@ class Solution
         vector<int> productExceptSelf(vector<int>& nums) 
         {
             int n = nums.size();
-            vector<int> dp_left(n, 1);
-            vector<int> dp_right(n, 1);
+            vector <int> ans(n, 1);
 
-            for (int i = 1; i < n; i++) 
+            for (int i = 1; i < n; i++)
             {
-                dp_left[i] = dp_left[i - 1] * nums[i - 1];
+                ans[i] = ans[i - 1] * nums[i - 1];
             }
 
-            for (int i = n - 2; i >= 0; i--) 
+            int product = 1;
+
+            for (int i = n - 1; i >= 0; i--)
             {
-                dp_right[i] = dp_right[i + 1] * nums[i + 1];
+                ans[i] = ans[i] * product;
+                product *= nums[i];
             }
 
-            vector<int> ans(n);
-            for (int i = 0; i < n; i++) 
-            {
-                ans[i] = dp_left[i] * dp_right[i];
-            }
-            
             return ans;
         }
 };

b3b16e7e83edd6e361416a2f2df0b2580666f551
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-03-20 07:35:48 +0100

    20-3-24

---
+++ MergeInBetweenLinkedLists/cpp.cpp
@@ -0,0 +1,27 @@
+class Solution 
+{
+    public:
+        ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) 
+        {
+            ListNode *a_prev = nullptr, *b_post = list1;
+            
+            for (int i = 0; i <= b; i++) 
+            {
+                if (i == a-1) a_prev = b_post;
+                if (i != b) b_post = b_post->next;
+            }
+            
+            a_prev->next = list2;
+            
+            ListNode* list2_end = list2;
+
+            while (list2_end->next != nullptr)
+            {
+                list2_end = list2_end->next;
+            }
+            
+            list2_end->next = b_post->next;
+            
+            return list1;
+        }
+};
